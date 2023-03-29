13ee07912156bdc1892689343611c9be34a48434
llaurelson <llaurelson@gmail.com>
2023-03-29 15:54:12 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -8,7 +8,7 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-#!/usr/bin/python3
+
 """This is the place class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
@@ -42,7 +42,71 @@ class Place(BaseModel, Base):
         price_by_night:: pice for a staying in int
         latitude: latitude in flaot
         longitude: longitude in float
-        amenity_ids: list of Amenity ids
+        amenity_ids: list of Amenity ids#!/usr/bin/python3#!/usr/bin/python3
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+
+#!/usr/bin/python3
+
+"""This is the review class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from models.base_model import BaseModel, Base
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+
+class Review(BaseModel, Base):
+
+    """This is the class for Review
+
+    Attributes:
+
+        place_id: place id
+
+        user_id: user id
+
+        text: review description
+
+    """
+
+    __tablename__ = "reviews"
+
+    text = Column(String(1024), nullable=False)
+
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+
+
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
