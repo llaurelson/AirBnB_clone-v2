07d3099cc5bb4359ccf4e95b2612f0e7ad40ef8c
llaurelson <llaurelson@gmail.com>
2023-04-01 13:13:46 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    name = ""
-#!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
