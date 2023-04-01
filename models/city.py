e33e32ae6e8f415ea70021233e5b3379f1cdd363
llaurelson <llaurelson@gmail.com>
2023-04-01 13:12:41 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
-#!/usr/bin/python3
 """This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
