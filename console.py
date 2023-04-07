ac309389d42c4929386e2eb9225c7951098f6ee3
llaurelson <llaurelson@gmail.com>
2023-04-07 11:47:33 +0100

    Update console.py

--- console.py
+++ console.py
@@ -326,5 +326,7 @@ class HBNBCommand(cmd.Cmd):
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
-if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+        
+        
+    if __name__ == "__main__":
+        HBNBCommand().cmdloop()
